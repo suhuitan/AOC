55fbb2e5fc389f737e43b2f55bcdfc4bf3898e36
Su Tan <stan@squaresapce.com>
2020-12-06 16:36:55 -0500

    Day 6

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -22,7 +22,7 @@ public class App {
     //  4) implement and run
 
     // CHANGE ME EVERYDAY!
-    int day = 5;
+    int day = 6;
 
     // you don't really need to touch anything else after this
     Day solution = (Day) instantiate(day);
---
+++ src/test/java/solutions/Day06Test.java
@@ -0,0 +1,52 @@
+package solutions;
+
+import static org.testng.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Day06Test {
+
+  String test = "abc\n"
+      + "\n"
+      + "a\n"
+      + "b\n"
+      + "c\n"
+      + "\n"
+      + "ab\n"
+      + "ac\n"
+      + "\n"
+      + "a\n"
+      + "a\n"
+      + "a\n"
+      + "a\n"
+      + "\n"
+      + "b"
+      + "\n";
+
+  private List<String> inputs;
+  private Day06 underTest = new Day06();
+
+  @BeforeMethod
+  public void setup() {
+    inputs = Arrays.asList(test.split("\n"));
+
+  }
+
+  @Test
+  public void testPart1() {
+    String max = underTest.part1(inputs);
+    Assert.assertEquals(max, "11");
+  }
+
+  @Test
+  public void testPart2() {
+    String max = underTest.part2(inputs);
+
+    Assert.assertEquals(max, "6");
+  }
+}
