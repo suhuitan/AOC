5122948e8027c8f1d0f1016e7f662ed0b91ffbc6
Su Tan <stan@squaresapce.com>
2020-12-11 13:50:07 -0500

    clean up Day 11

--- src/main/java/solutions/Day11.java
+++ src/main/java/solutions/Day11.java
@@ -1,6 +1,5 @@
 package solutions;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,72 +8,69 @@ import org.testng.internal.collections.Pair;
 public class Day11 implements Day {
 
   public static final List<Pair<Integer, Integer>> DIRECTIONS = Arrays
-      .asList(new Pair<>(-1, -1), new Pair<>(-1, 0), new Pair<>(-1, 1),
+      .asList(
+          new Pair<>(-1, -1), new Pair<>(-1, 0), new Pair<>(-1, 1),
           new Pair<>(0, -1), new Pair<>(0, 1),
-          new Pair<>(1, -1), new Pair<>(1, 0), new Pair<>(1, 1));
+          new Pair<>(1, -1), new Pair<>(1, 0), new Pair<>(1, 1)
+      );
 
   @Override
   public String part1(List<String> input) {
-    List<char[][]> seatMaps = new ArrayList<>();
-    seatMaps.add(processInput(input));
+    char[][] seatMap = processInput(input);
+    // padding grid so that i don't have to deal with edge cases
     int rowSize = input.size() + 2;
     int colSize = input.get(0).length() + 2;
 
     int flipped = 0;
-    int iteration = 0;
 
     do {
-      char[][] seatMap = seatMaps.get(iteration++);
       flipped = 0;
-      char[][] newMap = getBlank(rowSize, colSize);
+      char[][] newMap = new char[rowSize][colSize];
       for (int i = 1; i < seatMap.length - 1; i++) {
         for (int j = 1; j < seatMap[i].length - 1; j++) {
-          int occupancy = checkNeighbors(i, j, seatMap);
-          boolean hasFlipped = flipOrCopy(seatMap, newMap, i, j, occupancy, 4);
-          if (hasFlipped) {
-            flipped++;
-          }
-        }
-      }
-      seatMaps.add(newMap);
-    } while (flipped > 0);
-
-    //debug
-    printSeatMap(seatMaps.get(iteration));
-
-    return String.valueOf(countOccupied(seatMaps.get(iteration)));
-  }
-
-  @Override
-  public String part2(List<String> input) {
-    List<char[][]> seatMaps = new ArrayList<>();
-    seatMaps.add(processInput(input));
-    int rowSize = input.size() + 2;
-    int colSize = input.get(0).length() + 2;
-
-    int flipped = 0;
-    int iteration = 0;
-
-    do {
-      char[][] seatMap = seatMaps.get(iteration++);
-      flipped = 0;
-      char[][] newMap = getBlank(rowSize, colSize);
-      for (int i = 1; i < seatMap.length - 1; i++) {
-        for (int j = 1; j < seatMap[i].length - 1; j++) {
-          int occupancy = checkNeighbors2(i, j, seatMap);
+          int occupancy = checkNeighbors(i, j, seatMap, false);
           boolean hasFlipped = flipOrCopy(seatMap, newMap, i, j, occupancy, 5);
           if (hasFlipped) {
             flipped++;
           }
         }
       }
-      seatMaps.add(newMap);
+      seatMap=newMap;
     } while (flipped > 0);
 
     //debug
     // printSeatMap(seatMaps.get(iteration));
 
-    return String.valueOf(countOccupied(seatMaps.get(iteration)));
+    return String.valueOf(countOccupied(seatMap));
+  }
+
+  @Override
+  public String part2(List<String> input) {
+    char[][] seatMap = processInput(input);
+    int rowSize = input.size() + 2;
+    int colSize = input.get(0).length() + 2;
+
+    int flipped = 0;
+
+    do {
+      flipped = 0;
+      char[][] newMap = new char[rowSize][colSize];
+      for (int i = 1; i < seatMap.length - 1; i++) {
+        for (int j = 1; j < seatMap[i].length - 1; j++) {
+          int occupancy = checkNeighbors(i, j, seatMap,true);
+          boolean hasFlipped = flipOrCopy(seatMap, newMap, i, j, occupancy, 5);
+          if (hasFlipped) {
+            flipped++;
+          }
+        }
+      }
+      seatMap=newMap;
+    } while (flipped > 0);
+
+    //debug
+    // printSeatMap(seatMaps.get(iteration));
+
+    return String.valueOf(countOccupied(seatMap));
   }
 
   public boolean flipOrCopy(char[][] seatMap, char[][] newMap, int i, int j, int occupancy, int tolerance) {
@@ -92,13 +88,13 @@ public class Day11 implements Day {
     return false;
   }
 
-  private int checkNeighbors2(int i, int j, char[][] seatMap) {
+  private int checkNeighbors(int i, int j, char[][] seatMap, boolean searchDirection) {
     int occupied = 0;
 
     for (Pair<Integer, Integer> direction : DIRECTIONS) {
       int x = i + direction.first();
       int y = j + direction.second();
-      while (x > 0 && x < seatMap.length - 1 && y > 0 && y < seatMap[0].length - 1) {
+      do {
         if (seatMap[x][y] == '#') {
           occupied++;
           break;
@@ -107,7 +103,7 @@ public class Day11 implements Day {
         }
         x += direction.first();
         y += direction.second();
-      }
+      } while ( searchDirection && x > 0 && x < seatMap.length - 1 && y > 0 && y < seatMap[0].length - 1);
     }
 
     return occupied;
@@ -117,21 +113,6 @@ public class Day11 implements Day {
     for (int i = 0; i < chars.length; i++) {
       System.out.println(chars[i]);
     }
-  }
-
-  private int checkNeighbors(int i, int j, char[][] seatMap) {
-    int occupied = 0;
-
-    for (int x = i - 1; x <= i + 1; x++) {
-      for (int y = j - 1; y <= j + 1; y++) {
-        if (!(x == i && y == j)) {
-          if (seatMap[x][y] == '#') {
-            occupied++;
-          }
-        }
-      }
-    }
-    return occupied;
   }
 
   private char[][] processInput(List<String> input) {
@@ -149,10 +130,5 @@ public class Day11 implements Day {
         .flatMapToInt(String::chars)
         .filter(x -> x == 35)
         .count();
-  }
-
-  private char[][] getBlank(int rowSize, int colSize) {
-    char[][] paddedMap = new char[rowSize][colSize];
-    return paddedMap;
   }
 }
