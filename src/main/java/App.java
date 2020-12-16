600e310cc9bffae308750fccbcbab32f95d7c5ee
Su Tan <stan@squaresapce.com>
2020-12-16 02:27:43 -0500

    Day 16

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -22,7 +22,7 @@ public class App {
     //  4) implement and run
 
     // CHANGE ME EVERYDAY!
-    int day = 15;
+    int day = 16;
 
     // you don't really need to touch anything else after this
     Day solution = (Day) instantiate(day);
