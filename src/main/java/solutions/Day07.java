fd879e98eb2df1e7f0af8d0c5cf9f3b4c98c3992
Su Hui Tan <stan@squarespace.com>
2021-12-07 17:02:48 -0500

    Day 7

---
+++ src/main/java/solutions/Day07.java
@@ -0,0 +1,18 @@
+package solutions;
+
+import java.util.List;
+
+import utils.Day;
+
+public class Day07 implements Day {
+
+  @Override
+  public String part1(List<String> input) {
+    return null;
+  }
+
+  @Override
+  public String part2(List<String> input) {
+    return null;
+  }
+}
---
+++ src/test/java/solutions/Day07Test.java
@@ -0,0 +1,36 @@
+package solutions;
+
+import static utils.Utils.splitByNewLine;
+
+import java.util.List;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Day07Test {
+
+  private Day07 underTest;
+  private String input = "";
+
+  @BeforeMethod
+  public void setup() {
+    underTest = new Day07();
+  }
+
+  @Test
+  public void testPart1() {
+    List<String> inputs = splitByNewLine(input);
+    String result = underTest.part1(inputs);
+
+    Assert.assertEquals(result, null);
+  }
+
+  @Test
+  public void testPart2() {
+    List<String> inputs = splitByNewLine(input);
+    String result = underTest.part2(inputs);
+
+    Assert.assertEquals(result, null);
+  }
+}
